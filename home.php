5560f7f427a770e054c3fc8dd83704b4e94118bd
SumahanZ <83936130+SumahanZ@users.noreply.github.com>
2021-05-17 15:34:37 +0700

    first commit

---
+++ home.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Frootie</title>
+    <link rel="stylesheet" href="home.css">
+</head>
+<body>
+    <div class="container">
+    <div class="navigationbar">
+    <div class="logo">
+    <img src="images/Frootie_Logo.png">
+    </div>
+    <nav>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Products</a></li>
+            <li><a href="">Order</a></li>
+            <li><a href="">Contact Us</a></li>
+        </ul>
+    </nav>
+    </div>
+    <div class="row">
+        <div class="column">
+            <h1>Try Out Our New <br>Fresh Fruits!</h1>
+            <p>Our fruits are 100% authentic, breeded, and harvested from <br> 
+            the best selection of greenhouses in Indonesia.
+            <br><a href="" class="button">See Our Products</a>
+        </div>
+        <div class="column">
+            <img src="images/fruits.png">
+        </div>
+    </div>
+    
+    </div>
+    
+</body>
+</html>
